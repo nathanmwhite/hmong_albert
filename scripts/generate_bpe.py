8a5025ec347197371e02d5bc1ea31328b459dacb
Nathan M. White <nathan.white@mytwu.ca>
2021-01-01 10:56:38 +1000

    Completed byte-pair encoding script

--- scripts/generate_bpe.py
+++ scripts/generate_bpe.py
@@ -4,8 +4,11 @@ import os
 
 import sentencepiece as spm
 
-file_list = os.listdir() # assuming this is already in the sch_corpus folder;
-# will later need to update this with all relevant text files
+script_loc = os.path.dirname(__file__)
+files_loc = os.path.join(os.path.dirname(script_loc), 'albert_raw_data')
+
+# generates list of data files; sliced to -1 with topdown=False to exclude readme file in top folder
+file_list = [os.path.join(root, filename) for (root,_,files) in os.walk(files_loc, topdown=False) for filename in files][:-1]
 
 spm.SentencePieceTrainer.train(input=file_list, model_prefix='hm', vocab_size=50000, user_defined_symbols=[])
 
