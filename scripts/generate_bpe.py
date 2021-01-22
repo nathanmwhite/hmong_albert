03012bbcc924ce9522cdb1cf2e6623b61d99af39
Nathan M. White <nathan.white@mytwu.ca>
2021-01-22 14:12:56 +1000

    Added metadata to generate_bpe.py

--- scripts/generate_bpe.py
+++ scripts/generate_bpe.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python
 
+# Byte-pair encoding script for Hmong ALBERT project
+# Copyright (c) 2021 Hmong Medical Corpus Project
+# Author : Nathan M. White <nathan.white1@my.jcu.edu.au>
+"""
+This script generates byte-pair encodings from the raw data
+for the Hmong ALBERT model.
+"""
+
 import os
 
 import sentencepiece as spm
 
+# Copyright notice
+__copyright__ = "Copyright (c) 2021 Hmong Medical Corpus Project"
+
+# project_url
+__url__ = "https://corpus.ap-southeast-2.elasticbeanstalk.com/hminterface"
+
+# author
+__author__ = "Nathan M. White"
+__author_email__ = "nathan.white1@my.jcu.edu.au"
+
+# central function of script
 def generate_bpe(data_loc):
-	'''generate_bpe generates the byte-pair encodings based on
-	the data found at data_loc.
+	"""
+        Generates the byte-pair encodings based on the data found at 
+         data_loc.
 	@param data_loc : string consisting of the folder name containing
-	                   the data, without any separators'''
+	                   the data, without any separators
+        """
 
 	# determines the absolute filepath for the files
 	script_loc = os.path.dirname(__file__)
@@ -28,5 +49,6 @@ def generate_bpe(data_loc):
 								   user_defined_symbols=[])
 								
 if __name__ == '__main__':
+    # generate byte-pair encodings on both uncased and cased data
     generate_bpe('uncased_data')
     generate_bpe('cased_data')								
