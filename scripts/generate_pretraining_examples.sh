514bb5910a518bc2a26b292b30c14e2361b28ae2
Nathan M. White <nathan.white@mytwu.ca>
2021-01-26 21:43:10 +1000

    Added conditional directory statements

--- scripts/generate_pretraining_examples.sh
+++ scripts/generate_pretraining_examples.sh
@@ -2,7 +2,13 @@
 
 pip install -r albert/requirements.txt
 
-# cased data : 128 and 512
+# check if pretrain_examples folder exists, and create if not
+if [ ! -d "./cased_data/pretrain_examples" ]
+then
+	mkdir ./cased_data/pretrain_examples
+fi
+
+# cased data : generate for sequence lengths 128 and 512
 python -m albert.create_pretraining_data \
 	--input_file=./cased_data/pretrain/sch_corpus/*.*,\
                      ./cased_data/pretrain/knh/*.*,\
@@ -21,7 +27,13 @@ python -m albert.create_pretraining_data \
         --max_seq_length=512 \
         --max_predictions_per_seq=77
 
-# uncased data
+# check if pretrain_examples folder exists, and create if not
+if [ ! -d "./uncased_data/pretrain_examples" ]
+then
+	mkdir ./uncased_data/pretrain_examples
+fi
+
+# uncased data : generate for sequence lengths 128 and 512
 python -m albert.create_pretraining_data \
 	--input_file=./uncased_data/pretrain/sch_corpus/*.*,\
                      ./uncased_data/pretrain/knh/*.*,\
